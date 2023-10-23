40f80fe60cf3ae85fa11257e0b73c50178b7e4b0
gfreychet <gfreychet@gmail.com>
2023-10-23 13:48:49 +0200

    Change typo pos_R by pos_r

--- pygix/transform.py
+++ pygix/transform.py
@@ -836,7 +836,7 @@ class Transform(GrazingGeometry):
                     not self._lut_gi_transformer.default_ipl):
                         reset = "x_range was defined in LUT"
                     elif (x_range is not None) and \
-                            (self._lut_gi_transformer.pos0_Range != (
+                            (self._lut_gi_transformer.pos0_range != (
                             min(x_range), max(x_range) * EPS32)):
                         reset = ("x_range is defined"
                                  " but not the same as in LUT")
@@ -844,7 +844,7 @@ class Transform(GrazingGeometry):
                         y_range is None) and not self._lut_gi_transformer.default_opl:
                         reset = "y_range not defined and LUT had y_range defined"
                     elif (y_range is not None) and \
-                            (self._lut_gi_transformer.pos1_Range != (
+                            (self._lut_gi_transformer.pos1_range != (
                             min(y_range), max(y_range) * EPS32)):
                         reset = "y_range requested and LUT's y_range don't match"
                 error = False
@@ -966,7 +966,7 @@ class Transform(GrazingGeometry):
                             self._csr_gi_transformer.default_ipl:
                         reset = "x_range was defined in CSR"
                     elif (x_range is not None) and \
-                            (self._csr_gi_transformer.pos0Range != \
+                            (self._csr_gi_transformer.pos0_range != \
                             (min(x_range), max(x_range) * EPS32)):
                         reset = ("x_range is defined"
                                  " but not the same as in CSR")
@@ -975,7 +975,7 @@ class Transform(GrazingGeometry):
                         reset = ("y_range not defined and"
                                  " CSR had y_range defined")
                     elif (y_range is not None) and \
-                            (self._csr_gi_transformer.pos1Range != \
+                            (self._csr_gi_transformer.pos0_range != \
                             (min(y_range), max(y_range) * EPS32)):
                         reset = ("y_range requested and"
                                  " CSR's y_range don't match")
@@ -1500,14 +1500,14 @@ class Transform(GrazingGeometry):
                         p0_range is None) and not self._lut_gi_integrator.default_p0:
                         reset = "p0_range was defined in LUT"
                     elif (p0_range is not None) and \
-                            (self._lut_gi_integrator.pos0_Range != (
+                            (self._lut_gi_integrator.pos0_range != (
                             min(p0_range), max(p0_range) * EPS32)):
                         reset = "p0_range is defined but not the same as in LUT"
                     if (
                         p1_range is None) and not self._lut_gi_integrator.default_p1:
                         reset = "p1_range not defined and LUT had p1_range defined"
                     elif (p1_range is not None) and \
-                            (self._lut_gi_integrator.pos1_Range != (
+                            (self._lut_gi_integrator.pos1_range != (
                             min(p1_range), max(p1_range) * EPS32)):
                         reset = "p1_range requested and LUT's p1_range don't match"
                 if reset:
